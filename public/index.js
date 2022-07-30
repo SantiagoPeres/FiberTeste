676b1db4c67a8d491b5ba1d0a3a6527a4dc72deb
SantiagoPeres <santiagogperes@gmail.com>
2022-07-30 22:23:32 +0200

    commit

--- public/index.js
+++ public/index.js
@@ -1,15 +1,15 @@
-function removeFromDb(item){
-    fetch(`/delete?item=${item}`, {method: "Delete"}).then(res =>{
+function removeFromDb(username){
+    fetch(`/delete?username=${username}`, {method: "Delete"}).then(res =>{
         if (res.status == 200){
             window.location.pathname = "/"
         }
     })
  }
 
-function updateDb(item){
-	let input = document.getElementById(item)
-    let newitem = input.value
-    fetch(`/uptade?olditem=${item}&newitem=${newitem}`, {method:"PUT"}).then(res => {
+function updateDb(username){
+	let input = document.getElementById(username)
+    let newusername = input.value
+    fetch(`/uptade?oldusername=${username}&newusername=${newusername}`, {method:"PUT"}).then(res => {
         if (res.status == 200){
             alert("Database uptade")
                 window.location.pathname = "/"
